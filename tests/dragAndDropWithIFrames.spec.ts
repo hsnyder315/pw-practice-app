acbaa0abea9f5ef94409b41fbed0386517ebab0a
Hunter Snyder <hsnyder315@gmail.com>
2025-07-02 17:24:58 -0500

    Finished Section 5

--- tests/dragAndDropWithIFrames.spec.ts
+++ tests/dragAndDropWithIFrames.spec.ts
@@ -3,5 +3,16 @@ import {test, expect} from '@playwright/test'
 test('Drag and Drop with iFrames', async({page}) => {
     await page.goto('https://www.globalsqa.com/demo-site/draganddrop/')
 
-    await page.locator('li', {hasText: "High Tatras 2"}).click()
+    // Find, Select, and Move an image from the Photo Manager to the Trash
+    const frame = page.frameLocator('[rel-title="Photo Manager"] iframe')
+    await frame.locator('li', {hasText: "High Tatras 2"}).dragTo(frame.locator('#trash'))
+
+    // More precise control
+    await frame.locator('li', {hasText: "High Tatras 4"}).hover()
+    await page.mouse.down()
+    await frame.locator('#trash').hover()
+    await page.mouse.up()
+
+    // Finally make an assertion
+    await expect(frame.locator('#trash li h5')).toHaveText(["High Tatras 2", "High Tatras 4"])
 })
