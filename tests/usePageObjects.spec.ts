a1ac75008e7416f145c12e67f1d2470f6d47f7dc
Hunter Snyder <hsnyder315@gmail.com>
2025-07-04 11:08:23 -0500

    more page object

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,34 +1,38 @@
 // Section 6 Lesson 46 - 52: Page Objects
 import { expect, test } from '@playwright/test'
+import { PageManager } from '../page-objects/pageManager'
 // Created page-objects folder
 // After page object is created, import to appropriate files as seen here. Be sure to include export before class for any that you wish to import to a file.
-import { NavigationPage } from '../page-objects/navigationPage'
-import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
-import { DatepickerPage } from '../page-objects/datepickerPage'
+// import { NavigationPage } from '../page-objects/navigationPage' - commented out for section 6 lesson 51 as it's now being handled in pageManager.ts and the new import above
+// import { FormLayoutsPage } from '../page-objects/formLayoutsPage' - commented out for section 6 lesson 51 as it's now being handled in pageManager.ts and the new import above
+// import { DatepickerPage } from '../page-objects/datepickerPage' - commented out for section 6 lesson 51 as it's now being handled in pageManager.ts and the new import above
+
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/')
 })
 
 test('Navigation in Side Bar', async({page}) => {
+    const pm = new PageManager(page)
     // To use the new class, we need a new instance of the page object in the test
-    const navTo = new NavigationPage(page)
-    await navTo.formLayoutsPage()
-    await navTo.datepickerPage()
-    await navTo.smartTablePage()
-    await navTo.toastrPage()
-    await navTo.tooltipPage()
+    // const navTo = new NavigationPage(page) - commented out for section 6 lesson 51 as it's now being handled in pageManager.ts and the new const above
+    await pm.navigateTo().datepickerPage()
+    await pm.navigateTo().formLayoutsPage()
+    await pm.navigateTo().smartTablePage()
+    await pm.navigateTo().toastrPage()
+    await pm.navigateTo().tooltipPage()
 })
 
 test('Paramaterized Methods', async({page}) => {
-    const navigateTo = new NavigationPage(page)
-    const onFormLayoutsPage = new FormLayoutsPage(page)
-    const onDatepickerPage = new DatepickerPage(page)
+    const pm = new PageManager(page)
 
-    await navigateTo.formLayoutsPage()
-    await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 1')
-    await onFormLayoutsPage.submitUsingInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', true)
-    await navigateTo.datepickerPage()
-    await onDatepickerPage.selectCommonDatePickerDateFromToday(5)
+    // const navigateTo = new NavigationPage(page) - commented out for section 6 lesson 51 as it's now being handled in pageManager.ts and the new const above
+    // const onFormLayoutsPage = new FormLayoutsPage(page) - commented out for section 6 lesson 51 as it's now being handled in pageManager.ts and the new const above
+    // const onDatepickerPage = new DatepickerPage(page) - commented out for section 6 lesson 51 as it's now being handled in pageManager.ts and the new const above
 
-    await onDatepickerPage.selectDatePickerWithRangeFromToday(6, 15)
+    await pm.navigateTo().formLayoutsPage()
+    await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 1')
+    await pm.onFormLayoutsPage().submitUsingInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', true)
+    await pm.navigateTo().datepickerPage()
+    await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(5)
+    await pm.onDatepickerPage().selectDatePickerWithRangeFromToday(6, 15)
 })
