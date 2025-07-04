29dfd38caad55b16a797e40159dfcd25b823356f
Hunter Snyder <hsnyder315@gmail.com>
2025-07-04 10:51:24 -0500

    more page object

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,10 +1,10 @@
-// Section 6 Lesson 46: First Page Object
+// Section 6 Lesson 46 - 52: Page Objects
 import { expect, test } from '@playwright/test'
 // Created page-objects folder
 // After page object is created, import to appropriate files as seen here. Be sure to include export before class for any that you wish to import to a file.
 import { NavigationPage } from '../page-objects/navigationPage'
 import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
-
+import { DatepickerPage } from '../page-objects/datepickerPage'
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/')
 })
@@ -22,8 +22,13 @@ test('Navigation in Side Bar', async({page}) => {
 test('Paramaterized Methods', async({page}) => {
     const navigateTo = new NavigationPage(page)
     const onFormLayoutsPage = new FormLayoutsPage(page)
+    const onDatepickerPage = new DatepickerPage(page)
 
     await navigateTo.formLayoutsPage()
     await onFormLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 1')
     await onFormLayoutsPage.submitUsingInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', true)
+    await navigateTo.datepickerPage()
+    await onDatepickerPage.selectCommonDatePickerDateFromToday(5)
+
+    await onDatepickerPage.selectDatePickerWithRangeFromToday(6, 15)
 })
