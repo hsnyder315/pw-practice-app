cef6c63cdab96ff7b9ef83036e12fc4053d39a14
Hunter Snyder <hsnyder315@gmail.com>
2025-07-15 15:30:40 -0500

    added new dropdown helper and updated dropdown checks

--- page-objects/helperBase.ts
+++ page-objects/helperBase.ts
@@ -1,5 +1,5 @@
 // Section 6 Lesson 52: Parameterized Methods
-import { Page } from "@playwright/test";
+import { Page, expect } from "@playwright/test";
 
 export class HelperBase{
 
@@ -11,5 +11,24 @@ export class HelperBase{
 
     async waitForNumberOfSeconds(timeInSeconds: number){
         await this.page.waitForTimeout(timeInSeconds * 1000)
+    }
+}
+
+export class HelperUI {
+    // Add your helper methods here
+
+    static async selectAndVerifyOptions(
+        dropdown: import('@playwright/test').Locator,
+        optionList: import('@playwright/test').Locator,
+        options: string[]
+    ) {
+        await dropdown.click();
+        for (const option of options) {
+            await optionList.filter({ hasText: option }).click({ delay: 500 });
+            await expect(dropdown).toHaveText(option);
+            if (option !== options[options.length - 1]) {
+                await dropdown.click();
+            }
+        }
     }
 }
--- tests/buttonPressing.spec.ts
+++ tests/buttonPressing.spec.ts
@@ -1,10 +1,12 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
+import { HelperUI } from '../page-objects/helperBase';
 
 test.describe('Buttons', () => {
 
     test('IoT Dashboard', async ({ page }) => {
         await page.goto('/');
 
+        // On/Off Buttons
         const devices = [
             '.nb-lightbulb',
             '.nb-roller-shades',
@@ -30,59 +32,32 @@ test.describe('Dropdowns', () => {
         // Electricity Card
         const electDropDown = page.locator('ngx-electricity nb-select')
         const electList = page.locator('nb-option-list nb-option')
-        const electDates = ["week", "month","year"]
-
-        await electDropDown.click()
-        for(const date of electDates){
-            await electList.filter({hasText: date }).click({delay: 500})
-            await expect(electDropDown).toHaveText(date)
-            if(date !== "year")
-                await electDropDown.click()
-        }
+        const electDates = ["week", "month", "year"]
+        await HelperUI.selectAndVerifyOptions(electDropDown, electList, electDates)
 
         // Traffic Card
         const trafficDropDown = page.locator('ngx-traffic nb-select')
         const trafficList = page.locator('nb-option-list nb-option')
-        const trafficDates = ["week", "month","year"]
-
-        await trafficDropDown.click()
-        for(const date of trafficDates){
-            await trafficList.filter({hasText: date }).click({delay: 500})
-            await expect(trafficDropDown).toHaveText(date)
-            if(date !== "year")
-                await trafficDropDown.click()
-        }
+        const trafficDates = ["week", "month", "year"]
+        await HelperUI.selectAndVerifyOptions(trafficDropDown, trafficList, trafficDates)
+        
     })
 
     // Toastr Page
-    test('Toastr Dropdowns', async({page}) => {
+    test('Toastr Dropdowns v2', async({page}) => {
 
         await page.goto('../pages/modal-overlays/toastr')
 
         // Position
-        const positionDropdown = page.locator('[class="position-select appearance-outline size-medium status-basic shape-rectangle nb-transition"]')
-        const positionList = page.locator('nb-option-list nb-option')
-        const positionItems = ["top-right", "top-left", "bottom-left", "bottom-right", "top-end", "top-start", "bottom-end", "bottom-start"]
+        const positionDropdown = page.locator('[class="position-select appearance-outline size-medium status-basic shape-rectangle nb-transition"]');
+        const positionList = page.locator('nb-option-list nb-option');
+        const positionItems = ["top-right", "top-left", "bottom-left", "bottom-right", "top-end", "top-start", "bottom-end", "bottom-start"];
+        await HelperUI.selectAndVerifyOptions(positionDropdown, positionList, positionItems);
         
-        await positionDropdown.click()
-        for(const position of positionItems){
-            await positionList.filter({hasText: position}).click({delay: 500})
-            await expect(positionDropdown).toHaveText(position)
-            if(position !== "bottom-start")
-                await positionDropdown.click()
-        }
-
         // Toast Type
-        const toastTypeDropdown = page.locator('[class="appearance-outline size-medium status-basic shape-rectangle nb-transition"]')
-        const toastTypeList = page.locator('nb-option-list nb-option')
-        const toastTypeItems = ["primary", "success", "info", "warning", "danger"]
-        
-        await toastTypeDropdown.click()
-        for(const type of toastTypeItems){
-            await toastTypeList.filter({hasText: type}).click({delay: 500})
-            await expect(toastTypeDropdown).toHaveText(type)
-            if(type !== "danger")
-                await toastTypeDropdown.click()
-        }
+        const toastTypeDropdown = page.locator('[class="appearance-outline size-medium status-basic shape-rectangle nb-transition"]');
+        const toastTypeList = page.locator('nb-option-list nb-option');
+        const toastTypeItems = ["primary", "success", "info", "warning", "danger"];
+        await HelperUI.selectAndVerifyOptions(toastTypeDropdown, toastTypeList, toastTypeItems);
     })
 })
