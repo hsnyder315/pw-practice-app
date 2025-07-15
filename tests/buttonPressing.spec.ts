5d222c77a2cf94a84aeea007d5b913d5479a773d
Hunter Snyder <hsnyder315@gmail.com>
2025-07-15 09:45:17 -0500

    Update buttonPressing.spec.ts

--- tests/buttonPressing.spec.ts
+++ tests/buttonPressing.spec.ts
@@ -1,33 +1,88 @@
-import { test } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 test.describe('Buttons', () => {
 
-    test('IoT Dashboard Buttons', async({page}) => {
+    test('IoT Dashboard', async ({ page }) => {
+        await page.goto('/');
 
-        await page.goto('/')
+        const devices = [
+            '.nb-lightbulb',
+            '.nb-roller-shades',
+            '.nb-coffee-maker',
+            '.nb-audio',
+        ];
 
-        // Lightbulb Button
-        await page.locator('.nb-lightbulb').click() // - Turn Off
-        await page.locator('.nb-lightbulb').click() // - Turn On
-
-        // Roller Shades Button
-        await page.locator('.nb-roller-shades').click() // - Turn Off
-        await page.locator('.nb-roller-shades').click() // - Turn On
-
-        // Coffee Maker
-        await page.locator('.nb-coffee-maker').click() // - Turn Off
-        await page.locator('.nb-coffee-maker').click() //- Turn On
-
-        // Wireless Audio
-        await page.locator('.nb-audio').click() // - Turn Off
-        await page.locator('.nb-audio').click() // - Turn On
-
-    })
+        for (const selector of devices) {
+            const device = page.locator(selector);
+            await device.click({ delay: 600 }); // Turn Off
+            await device.click({ delay: 600 }); // Turn On
+        }
+    });
 })
 
 test.describe('Dropdowns', () => {
     
-    test('IoT Dashboard Dropdowns', async({page}) => {
+    // IoT Dashboard Page
+    test('Dashboard Dropdowns', async({page}) => {
 
+        await page.goto('/')
+
+        // Electricity Card
+        const electDropDown = page.locator('ngx-electricity nb-select')
+        const electList = page.locator('nb-option-list nb-option')
+        const electDates = ["week", "month","year"]
+
+        await electDropDown.click()
+        for(const date of electDates){
+            await electList.filter({hasText: date }).click({delay: 500})
+            await expect(electDropDown).toHaveText(date)
+            if(date !== "year")
+                await electDropDown.click()
+        }
+
+        // Traffic Card
+        const trafficDropDown = page.locator('ngx-traffic nb-select')
+        const trafficList = page.locator('nb-option-list nb-option')
+        const trafficDates = ["week", "month","year"]
+
+        await trafficDropDown.click()
+        for(const date of trafficDates){
+            await trafficList.filter({hasText: date }).click({delay: 500})
+            await expect(trafficDropDown).toHaveText(date)
+            if(date !== "year")
+                await trafficDropDown.click()
+        }
+    })
+
+    // Toastr Page
+    test('Toastr Dropdowns', async({page}) => {
+
+        await page.goto('../pages/modal-overlays/toastr')
+
+        // Position
+        const positionDropdown = page.locator('[class="position-select appearance-outline size-medium status-basic shape-rectangle nb-transition"]')
+        const positionList = page.locator('nb-option-list nb-option')
+        const positionItems = ["top-right", "top-left", "bottom-left", "bottom-right", "top-end", "top-start", "bottom-end", "bottom-start"]
+        
+        await positionDropdown.click()
+        for(const position of positionItems){
+            await positionList.filter({hasText: position}).click({delay: 500})
+            await expect(positionDropdown).toHaveText(position)
+            if(position !== "bottom-start")
+                await positionDropdown.click()
+        }
+
+        // Toast Type
+        const toastTypeDropdown = page.locator('[class="appearance-outline size-medium status-basic shape-rectangle nb-transition"]')
+        const toastTypeList = page.locator('nb-option-list nb-option')
+        const toastTypeItems = ["primary", "success", "info", "warning", "danger"]
+        
+        await toastTypeDropdown.click()
+        for(const type of toastTypeItems){
+            await toastTypeList.filter({hasText: type}).click({delay: 500})
+            await expect(toastTypeDropdown).toHaveText(type)
+            if(type !== "danger")
+                await toastTypeDropdown.click()
+        }
     })
 })
